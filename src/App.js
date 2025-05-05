fa6f50d2ca5b4042f6e73d5abf2c2083357325da
subha jha <subha.jha20@gmail.com>
2025-05-05 19:49:05 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // src/App.js
 import React from 'react';
-import ComingSoon from './components/ComingSoon/ComingSoon';
-// import Home from './pages/Home'
+// import ComingSoon from './components/ComingSoon/ComingSoon';
+import Home from './pages/Home'
 import SiteSettings from './components/SiteSettings/siteSettings';
 // import './App.css' 
 
 function App() {
   return (
     <div className="App" >
-     <ComingSoon/>
-       {/* <Home   />  */}
+     {/* <ComingSoon/> */}
+       <Home   /> 
        <SiteSettings />
     </div>
   );
