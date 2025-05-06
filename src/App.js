1acdc0f2bfaac75e7f927e78abc1df7bbdaac498
subha jha <subha.jha20@gmail.com>
2025-05-06 20:30:28 +0530

    header footer added for all the pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 // import ComingSoon from './components/ComingSoon/ComingSoon';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './pages/Home'
 import Register from './pages/Register';
 import StudentRegister from './pages/StudentRegister';
@@ -11,13 +13,15 @@ import SiteSettings from './components/SiteSettings/siteSettings';
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/student/register" element={<StudentRegister />} />
-      </Routes>
-      <SiteSettings />
-    </>
+    <Header /> 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/student/register" element={<StudentRegister />} />
+    </Routes>
+    <Footer /> 
+    <SiteSettings />
+  </>
   );
   // return (
   //   <div className="App" >
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Import all homepage sections
-import Header from '../components/Header/Header';
+// import Header from '../components/Header/Header';
 import Hero from '../components/Hero/Hero';
 import FeaturesSection from '../components/FeaturesSection/FeaturesSection';
 import ProgramsSection from '../components/ProgramsSection/ProgramsSection';
@@ -10,12 +10,12 @@ import TestimonialSection from '../components/TestimonialSection/TestimonialSect
 import ExpertSection from '../components/ExpertSection/ExpertSection';
 import BlogPreviewSection from '../components/BlogPreviewSection/BlogPreviewSection';
 import ContactCTA from '../components/ContactCTA/ContactCTA';
-import Footer from '../components/Footer/Footer';
+// import Footer from '../components/Footer/Footer';
 
 const Home = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Hero />
       <FeaturesSection />
       <ProgramsSection />
@@ -24,7 +24,7 @@ const Home = () => {
       <ExpertSection />
       <BlogPreviewSection />
       <ContactCTA />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
