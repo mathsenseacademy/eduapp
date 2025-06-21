45f2a6c431f5e16c2a1eec36a062fe610880c01e
Abhishek Nath <oovnath2@gmail.com>
2025-06-21 21:53:16 +0530

    course detail HTML

--- src/components/AdminPanel/Courses/CourseDetails.jsx
+++ src/components/AdminPanel/Courses/CourseDetails.jsx
@@ -26,23 +26,24 @@ export default function CourseDetails() {
     <div className="course-details-container my-5">
       <div className="row align-items-center gx-0">
         {/* Left bubbles */}
-        <div className="col-12 col-md-6 position-relative mb-4 mb-md-0">
-          {/* Purple bubble */}
-          <div className="bubble bubble--purple">
-            <h1 className="bubble__title">{course.course_name}</h1>
-          </div>
+        <div className="col-12 col-md-6 position-relative mb-4 mb-md-0 Left-bubbles">
+          {/* <div className="Left-bubbles"> */}
+          <div className="Left-section">
+            {/* Blue bubble */}
+            <div className="bubble bubble--blue">
+              <h1 className="bubble__title">{course.course_name}</h1>
+            </div>
 
-          {/* Green bubble */}
-          <div className="bubble bubble--green">
-            <p className="bubble__text">
-              {course.course_subtitle ||
-                "Engaging, expert-led online math programs for kids from Grade"}
-            </p>
-            <button className="bubble__btn">Enroll Now</button>
-          </div>
+            {/* Orange bubble */}
+            <div className="bubble bubble--orange">
+              <p className="bubble__text">
+                {course.course_subtitle ||
+                  "Engaging, expert-led online math programs for kids from Grade"}
+              </p>
+              <button className="bubble__btn">Enroll Now</button>
+            </div>
 
-          {/* Circular arrow button */}
-          <button
+            <button
             className="circle-arrow"
             onClick={() => {
               /* e.g. navigate to next slide */
@@ -51,12 +52,30 @@ export default function CourseDetails() {
             {/* ▶ */}
             ...
           </button>
+
+          </div>
+          {/* Purple bubble */}
+          {/* <div className="bubble bubble--purple">
+            <h1 className="bubble__title">{course.course_name}</h1>
+          </div> */}
+
+          {/* Green bubble */}
+          {/* <div className="bubble bubble--green">
+            <p className="bubble__text">
+              {course.course_subtitle ||
+                "Engaging, expert-led online math programs for kids from Grade"}
+            </p>
+            <button className="bubble__btn">Enroll Now</button>
+          </div> */}
+
+          {/* Circular arrow button */}
+          
         </div>
 
         {/* Right side: logo + students */}
         <div className="col-12 col-md-6 text-center">
           <div className="logo-wrapper mb-4">
-            <img src={logo} alt="MathSense Academy" className="logo-img" />
+            <img src={logo} alt="MathSense Academy" className="hero-logo-img" />
           </div>
 
           <div className="students-wrapper d-flex justify-content-center gap-3">
